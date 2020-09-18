80d8cf26c2b3f5c8102da4f694eb688f3fdd2156
Anton Borries <anton.borries@2denker.de>
2020-09-18 10:18:11 +0200

    Revert vsync Renaming

--- lib/src/panel.dart
+++ lib/src/panel.dart
@@ -230,7 +230,7 @@ class _SlidingUpPanelState extends State<SlidingUpPanel> with SingleTickerProvid
     _vt = new VelocityTracker(widget.pointerDeviceKind);
 
     _ac = new AnimationController(
-      TickerProvider: this,
+      vsync: this,
       duration: const Duration(milliseconds: 300),
       value: widget.defaultPanelState == PanelState.CLOSED ? 0.0 : 1.0 //set the default panel state (i.e. set initial value of _ac)
     )..addListener((){
